c4801f3194d20f4e0fe994c3dbea54df2a594085
rdivilbiss <rdivilbiss@d7630616-094b-11df-b8b0-ddcda0e0916c>
2010-02-22 00:43:51 +0000

    to test loginGlobals.php and has all e-mail blocks from login system pages.

---
+++ php/include/testLoginGlobals.php
@@ -0,0 +1,18 @@
+<?PHP
+//include "loginGlobals.php";
+?>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Test loginGlobals.php</title>
+</head>
+
+<body>
+<?PHP
+//echo lg_phrase_registration_mail6;
+echo "hello world";
+?>
+</body>
+
+</html>
