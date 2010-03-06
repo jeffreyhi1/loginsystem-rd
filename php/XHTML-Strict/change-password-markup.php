ec1afe5b21bc2f2f6c9130dfa522b8f1f48b9cdd
rdivilbiss <rdivilbiss@d7630616-094b-11df-b8b0-ddcda0e0916c>
2010-03-06 01:02:02 +0000

    correction to $_SESSION variable.

--- php/XHTML-Strict/change-password-markup.php
+++ php/XHTML-Strict/change-password-markup.php
@@ -1,15 +1,15 @@
           <div id="login-system">
 			<h1><?PHP echo lg_term_change_password ?></h1>
-			<h2><?PHP echo Session("name") ?></h2>
+			<h2><?PHP echo $_SESSION["name"] ?></h2>
 			<?PHP if ($message != lg_phrase_password_changed) { ?>
 				<div id="message"><?PHP echo $message ?></div>
-				<form id="frm" name="frm" method="post" action="<?PHP echo $lg_filename ?>" onsubmit="return validate(this);">
+				<form id="frm" method="post" action="<?PHP echo $lg_filename ?>" onsubmit="return validate(this);">
 				<fieldset>
 				  <legend><?PHP echo lg_term_change_password ?></legend>
-				  <label for="oldpassword"><?PHP echo lg_term_current_password ?>&nbsp;</label><br><input id="oldpassword" name="oldpassword" type="password" size="25" maxlength="255" title="<?PHP echo lg_phrase_oldpassword_title ?>"><span class="field_normal"><?PHP echo lg_term_required ?></span><br>
-				  <label for="newpassword"><?PHP echo lg_term_password ?>&nbsp;</label><br><input id="password" name="password" type="password" size="25" maxlength="255" title="<?PHP echo lg_phrase_password_title ?>"><span class="field_normal"><?PHP echo lg_term_required ?></span><br>
-				  <label for="confirm"><?PHP echo lg_term_confirm ?>&nbsp;</label><br><input id="confirm" name="confirm" type="password" size="25" maxlength="255" title="<?PHP echo lg_phrase_confirm_title ?>"><span class="field_normal"><?PHP echo lg_term_required ?></span><br>
-				  <?PHP echo writeToken() ?><input id="submit" name="submit" type="submit" value="<?PHP echo lg_term_change_password_button_text ?>">
+				  <label for="oldpassword"><?PHP echo lg_term_current_password ?>&nbsp;</label><br /><input id="oldpassword" name="oldpassword" type="password" size="25" maxlength="255" title="<?PHP echo lg_phrase_oldpassword_title ?>" /><span class="field_normal"><?PHP echo lg_term_required ?></span><br />
+				  <label for="newpassword"><?PHP echo lg_term_password ?>&nbsp;</label><br /><input id="password" name="password" type="password" size="25" maxlength="255" title="<?PHP echo lg_phrase_password_title ?>" /><span class="field_normal"><?PHP echo lg_term_required ?></span><br />
+				  <label for="confirm"><?PHP echo lg_term_confirm ?>&nbsp;</label><br /><input id="confirm" name="confirm" type="password" size="25" maxlength="255" title="<?PHP echo lg_phrase_confirm_title ?>" /><span class="field_normal"><?PHP echo lg_term_required ?></span><br />
+				  <?PHP echo writeToken(); ?><input id="submit" name="submit" type="submit" value="<?PHP echo lg_term_change_password_button_text ?>" />
 				</fieldset>
 				</form>
 			<?PHP }else{ ?>
