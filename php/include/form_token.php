5ac7a7fc131e7a1dbac500f3ca129990dd5a211e
rdivilbiss <rdivilbiss@d7630616-094b-11df-b8b0-ddcda0e0916c>
2010-03-09 01:46:53 +0000

    Had an ampersand where a period was needed for concatenation. Also had no viable salt, and was calling the Windows specific GUID function.

--- php/include/form_token.php
+++ php/include/form_token.php
@@ -11,14 +11,13 @@ function checkToken() {
 	$page=$_SERVER["SCRIPT_NAME"];
 	$oldToken=$_POST["token"];
 	$tokenStr = "IP:" . $_SESSION["ip"] . ",SESSIONID:" . session_id() . ",GUID:" . $_SESSION["guid"];
-	$testToken=sha1(($tokenStr&$_SESSION["salt"]).$_SESSION["salt"]);
+	$testToken=sha1(($tokenStr.$_SESSION["salt"]).$_SESSION["salt"]);
 	$checkToken=False;
 	If ($oldToken===$testToken) {
-	    $diff = time() - $_SESSION["time"]; 
+	    $diff = time() - $_SESSION["time"];
 		If ($diff<=300) { // Five minutes max
 	    	If ($_SESSION["usecookie"]) {
 			    If ($_COOKIE["token"]===$oldToken) {
-			    	setcookie("token", '', time()-42000);
 					return true;
 				}else{
 					$_SESSION = array();
@@ -26,8 +25,14 @@ function checkToken() {
 	    				setcookie(session_name(), '', time()-42000, '/');
 					}
 					session_destroy();
-					header("Location: http://www.rodsdot.com/oops.asp?p=" . $page . "&t=ec");
+					header("Location: http://". lg_domain . lg_form_error ."?p=" . $page . "&t=ec");
 				}
+				if (isset($_COOKIE["token"])) {
+					setcookie("token", '', time()-42000);
+				}
+				if (isset($_SESSION["usecookie"])) {
+					unset($_SESSION["usecookie"]);
+				}	
 			}else{	
 	  			return True;
 	  		}	
@@ -37,7 +42,7 @@ function checkToken() {
     			setcookie(session_name(), '', time()-42000, '/');
 			}
 			session_destroy();
-			header("Location: http://www.rodsdot.com/oops.asp?p=" . $page . "&t=et");
+			header("Location: http://". lg_domain . lg_form_error ."?p=" . $page . "&t=et");
 		}
 	}else{
 		$_SESSION = array();
@@ -45,28 +50,34 @@ function checkToken() {
     		setcookie(session_name(), '', time()-42000, '/');
 		}
 		session_destroy();
-		header("Location: http://www.rodsdot.com/oops.asp?p=" . $page . "&t=e");
+		header("Location: http://". lg_domain . lg_form_error ."?p=" . $page . "&t=e");
 	}
 }
-	
-function writeToken() {
+
+
+function writeToken($type = "X") {
 	/*****************************************************************************************
 	* Create and set a new token for CSRF protection
+	* Default output is an XHTML compatible input field. If $type = "H" then an HTML field
 	* on initial entry or after form errors and we are going to redisplay the form.
 	******************************************************************************************/
 	$salt="";
 	$tokenStr="";
-	$salt = sha1("rodsdot.com");
+	$salt = $salt = substr(md5(uniqid(rand(), true)), 0, 16);
 	setcookie("token", "", time()-42000);
 	$_SESSION["salt"]=$salt;
-	$_SESSION["guid"] = com_create_guid();
+	$_SESSION["guid"] = getGUID();
 	$_SESSION["ip"] = $_SERVER["REMOTE_ADDR"];
 	$_SESSION["time"] = time();
 	$tokenStr = "IP:" . $_SESSION["ip"] . ",SESSIONID:" . session_id() . ",GUID:" . $_SESSION["guid"];
-	$_SESSION["token"]=sha1(($tokenStr&$_SESSION["salt"]).$_SESSION["salt"]);
+	$_SESSION["token"]=sha1(($tokenStr.$_SESSION["salt"]).$_SESSION["salt"]);
 	if (setcookie("token", $_SESSION["token"], time()+500)) {
 		$_SESSION["usecookie"]=True;
 	}
-	echo '<input id="token" name="token" type="hidden" accesskey="u" tabindex="999" value="' .$_SESSION['token']. '">';
+	if (($type=="H") || ($type=="h")) {
+		return '<input id="token" name="token" type="hidden" accesskey="u" tabindex="999" value="' .$_SESSION['token']. '">';
+	}else{	
+		return '<input id="token" name="token" type="hidden" accesskey="u" tabindex="999" value="' .$_SESSION['token']. '" />';
+	}	
 }
 ?>
