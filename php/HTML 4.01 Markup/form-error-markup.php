8bd5ac449b4e83dc11176ae4139b794319177634
rdivilbiss <rdivilbiss@d7630616-094b-11df-b8b0-ddcda0e0916c>
2010-04-25 21:17:37 +0000

    25 APR 2010 alpha 0.1a

---
+++ php/HTML 4.01 Markup/form-error-markup.php
@@ -0,0 +1,6 @@
+			<!-- HTML 4.01 Strict -->
+			<!-- 25 APR 2010 alpha 0.1a -->
+			<div id="login-system">
+			  <div id="message"><?PHP echo $message?></div>
+			  <p><a href="<?PHP echo $page?>"><?PHP echo lg_phrase_logout_continue?></a></p>
+			</div>
---
+++ php/HTML 4.01 Markup/issue-verification-token-markup.php
@@ -0,0 +1,20 @@
+			<!-- HTML 4.01 Strict -->
+			<!-- 25 APR 2010 alpha 0.1a -->
+			<div id="login-system">
+				<h1><?PHP echo lg_term_issue_verification_token?></h1>
+				<?PHP if ($message==lg_phrase_issue_new_token) { ?>
+				<div id="message"><?PHP echo $message?></div>	
+				<div id="formDiv">
+					<form id="frm" method="post" action="<?PHP echo $lg_filename?>" onsubmit="return validate(this);">
+						<fieldset>
+						  <legend><?PHP echo lg_term_issue_verification_token?></legend>
+						  <p><label for="userid"><?PHP echo lg_term_userid?></label><br><input type="text" id="userid" name="userid" title="<?PHP echo lg_phrase_userid_title?>" size="50" maxlength="50"><br>
+						  <label for="email"><?PHP echo lg_term_email?></label><br><input type="text" id="email" name="email" title="<?PHP echo lg_phrase_email_title?>" size="50" maxlength="255"><br>
+						  <input type="submit" value="<?PHP echo lg_term_submit?>"><?PHP echo writeTokenH(); ?></p>
+						</fieldset>
+					</form>
+				</div>
+				<?PHP }else{ ?>
+				<div id="message"><?PHP echo $message?></div>
+				<?PHP } ?>
+			</div>
