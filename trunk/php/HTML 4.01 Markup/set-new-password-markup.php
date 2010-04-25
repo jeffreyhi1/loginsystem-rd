a1b7c73d2722f1505af95dc4bb16d8f880652cdb
rdivilbiss <rdivilbiss@d7630616-094b-11df-b8b0-ddcda0e0916c>
2010-04-25 21:17:37 +0000

    25 APR 2010 alpha 0.1a

---
+++ trunk/php/HTML 4.01 Markup/set-new-password-markup.php
@@ -0,0 +1,39 @@
+			<!-- HTML 4.01 Strict -->
+			<!-- 25 APR 2010 alpha 0.1a -->
+			<div id="login-system">
+			<h1><?PHP echo lg_term_reset_password ?></h1>
+			<?PHP if ($_SESSION["action"]=="token") { ?>
+			<div id="message"><?PHP echo $message ?></div>	
+			<div id="formDiv">
+			<form id="frm1" method="post" action="<?PHP echo $lg_filename ?>" onsubmit="return validate(this);">
+			<fieldset>
+			  <legend><?PHP echo lg_term_registration_verification ?></legend>
+			  <p><label for="resettoken"><?PHP echo lg_phrase_enter_unlock_code ?></label><br><input type="text" id="resettoken" name="resettoken" size="50" maxlength="64"><br>
+			  <input type="submit" value="<?PHP echo lg_term_submit ?>"><?PHP echo writeTokenH() ?><input name="changePassword" type="hidden" value=""></p>
+			</fieldset>
+			</form>
+			</div>
+			<?PHP 
+			}else{
+				if (($message!=lg_phrase_set_new_password_success) && ($_SESSION["action"]!="token")) {
+			 ?>
+			<div id="message"><?PHP echo $message ?></div>	
+			<div id="formDiv">
+			<form id="frm2" method="post" action="<?PHP echo $lg_filename ?>" onsubmit="return validate(this);">
+			<fieldset>
+			  <legend><?PHP echo lg_term_set_new_password ?></legend>
+			  <p><label for="newpassword"><?PHP echo lg_term_new_password ?></label><br><input id="newpassword" name="newpassword" type="password" size="50" maxlength="255"><br>
+			  <label for="newpassword"><?PHP echo lg_term_confirm ?></label><br><input id="confirm" name="confirm" type="password" size="50" maxlength="255"><br>
+			  <input type="submit" value="<?PHP echo lg_term_submit ?>"><?PHP echo writeTokenH() ?><input name="changePassword" type="hidden" value="1"><input type="hidden" name="resettoken" value="<?PHP echo $resettoken ?>"></p>
+			</fieldset>
+			</form>
+			</div>
+			<?PHP
+				}else{  // must be success or error, in either case we just show a message.
+			 ?>
+			<div id="message"><?PHP echo $message ?></div>
+			<?PHP 
+				}
+			} 
+			?>
+			</div>
