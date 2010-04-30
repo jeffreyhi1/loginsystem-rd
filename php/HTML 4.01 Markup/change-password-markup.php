598832f6ef739bd368e2fd0396d2e1822cf8e798
rdivilbiss <rdivilbiss@d7630616-094b-11df-b8b0-ddcda0e0916c>
2010-04-30 02:21:44 +0000

    Corrected Issues 38, 40, 41, 42

--- php/HTML 4.01 Markup/change-password-markup.php
+++ php/HTML 4.01 Markup/change-password-markup.php
@@ -11,9 +11,9 @@
 				<form id="frm" method="post" action="<?PHP echo $lg_filename; ?>" onsubmit="return validate(this);">
 				<fieldset>
 				  <legend><?PHP echo lg_term_change_password; ?></legend>
-				  <label for="oldpassword"><?PHP echo lg_term_current_password; ?>&nbsp;</label><br><input id="oldpassword" name="oldpassword" type="password" size="25" maxlength="255" title="<?PHP echo lg_phrase_oldpassword_title; ?>">&nbsp;<span class="field_normal"><?PHP echo lg_term_required; ?></span><br>
-				  <label for="newpassword"><?PHP echo lg_term_new_password; ?>&nbsp;</label><br><input id="password" name="password" type="password" size="25" maxlength="255" title="<?PHP echo lg_phrase_password_title; ?>">&nbsp;<span class="field_normal"><?PHP echo lg_term_required; ?></span><br>
-				  <label for="confirm"><?PHP echo lg_term_confirm; ?>&nbsp;</label><br><input id="confirm" name="confirm" type="password" size="25" maxlength="255" title="<?PHP echo lg_phrase_confirm_title; ?>">&nbsp;<span class="field_normal"><?PHP echo lg_term_required; ?></span><br>
+				  <label for="oldpassword"><?PHP echo lg_term_current_password; ?>&nbsp;</label><br><input id="oldpassword" name="oldpassword" type="password" size="25" maxlength="255" autocomplete="off" title="<?PHP echo lg_phrase_oldpassword_title; ?>">&nbsp;<span class="field_normal"><?PHP echo lg_term_required; ?></span><br>
+				  <label for="newpassword"><?PHP echo lg_term_new_password; ?>&nbsp;</label><br><input id="password" name="password" type="password" size="25" maxlength="255" autocomplete="off" title="<?PHP echo lg_phrase_password_title; ?>">&nbsp;<span class="field_normal"><?PHP echo lg_term_required; ?></span><br>
+				  <label for="confirm"><?PHP echo lg_term_confirm; ?>&nbsp;</label><br><input id="confirm" name="confirm" type="password" size="25" maxlength="255" autocomplete="off" title="<?PHP echo lg_phrase_confirm_title; ?>">&nbsp;<span class="field_normal"><?PHP echo lg_term_required; ?></span><br>
 				  <?PHP writeTokenH(); ?><input id="submit" name="submit" type="submit" value="<?PHP echo lg_term_change_password_button_text; ?>">
 				</fieldset>
 				</form>
