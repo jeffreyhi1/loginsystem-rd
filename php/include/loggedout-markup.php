737546d3b48880a1cad4ab8d51664450fe84021e
rdivilbiss <rdivilbiss@d7630616-094b-11df-b8b0-ddcda0e0916c>
2010-04-30 03:00:05 +0000

    Corrected issues 38 and 40 or removed the last update date from HTML comments

--- php/include/loggedout-markup.php
+++ php/include/loggedout-markup.php
@@ -2,7 +2,7 @@
 // $Id$
 ?>
 			<!-- XHTML 1.1 Strict -->
-			<!-- 27 APR 2010 alpha 0.1b debug -->
+			<!-- alpha 0.1b debug -->
 			<div id="login-system">
         	<h1><?PHP echo lg_phrase_logged_out; ?></h1>
 			<p><a href="<?PHP echo lg_home; ?>" title="<?PHP echo lg_phrase_logout_continue; ?>"><?PHP echo lg_phrase_logout_continue; ?></a></p>
