a037eedc718b507e24c1e842dafebcb93ddf61a5
notbono <notbono@gmail.com>
2010-05-01 19:38:02 +0000

    Class for all data access based on dbproviderfactory and configuration all moved to web.config.

---
+++ VB.NET/Login-System/Login-System/test.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data
+
+Module test
+    Public Sub test()
+        Using da As New DataAccess
+            Dim ds As DataSet = da.GetDataset("Select * from securityquestions")
+            For Each row As DataRow In ds.Tables(0).Rows
+                Debug.WriteLine(row(1))
+            Next
+        End Using
+    End Sub
+
+
+End Module
