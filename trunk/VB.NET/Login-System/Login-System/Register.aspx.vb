09d8cbd2708d31a2e3c277147d24ed719a3a5b08
notbono@gmail.com <notbono@gmail.com@d7630616-094b-11df-b8b0-ddcda0e0916c>
2010-05-02 20:03:18 +0000

    Started work on register page

---
+++ trunk/VB.NET/Login-System/Login-System/Register.aspx.vb
@@ -0,0 +1,18 @@
+﻿Public Partial Class Register
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        '"INSERT INTO users (dateRegistered, userid, password, name, email, ip, region, city, country, useragent, website, news, locked, dateLocked, token) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+
+    End Sub
+
+    Public Function PageTitle() As String
+        Return ResString("lg_term_registration")
+    End Function
+
+    Public Function Message() As String
+        Return "<strong>" & ResString("lg_term_please_register") & "</strong>"
+    End Function
+
+End Class
