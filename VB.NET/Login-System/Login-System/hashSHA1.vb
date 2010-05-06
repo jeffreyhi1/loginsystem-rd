59d337edd4139ca0476088b9362aaef6807c40c9
notbono <notbono@gmail.com>
2010-05-06 23:25:52 +0000

    Added support for SHA1 hashing.  Todo: test compatibility with asp version.

---
+++ VB.NET/Login-System/Login-System/hashSHA1.vb
@@ -0,0 +1,16 @@
+﻿Module hashSHA1
+    Function HashEncode(ByVal ToHash As String) As String
+        Dim Sha1Obj As New System.Security.Cryptography.SHA1CryptoServiceProvider
+        Dim bytes() As Byte = System.Text.Encoding.UTF8.GetBytes(ToHash)
+
+        bytes = Sha1Obj.ComputeHash(bytes)
+        Dim Result As New System.Text.StringBuilder
+
+        For Each b As Byte In bytes
+            Result.Append(b.ToString("x2"))
+        Next
+
+        Return Result.ToString.Substring(0, 40)
+    End Function
+
+End Module
