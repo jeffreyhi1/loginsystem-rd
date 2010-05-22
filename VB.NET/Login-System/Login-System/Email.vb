64021a30e3e90148c8be1013604dbd4db2d9f782
notbono <notbono@gmail.com>
2010-05-22 18:07:34 +0000

    Added support for password entropy and the token email. Added to the client side validation. Adapted more appsettings.

---
+++ VB.NET/Login-System/Login-System/Email.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Net.Mail
+
+Public Class Email
+
+
+    Public Shared Sub SendUnlockTokenEmail(ByVal Token As String, ByVal Email As String, ByVal Name As String, ByVal Destination As String)
+
+        Dim mailBody As String = String.Empty
+        mailBody = mailBody & "<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 4.0 Transitional//EN"">"
+        mailBody = mailBody & "<HTML><HEAD><META http-equiv=Content-Type content=""text/html; charset=us-ascii"">"
+        mailBody = mailBody & "</HEAD><BODY><DIV><FONT face=Arial size=2>" & ResString("lg_term_register_confirmation") & "<br><br>"
+        mailBody = mailBody & ResString("lg_term_to") & " " & Name & "<br><br>"
+        mailBody = mailBody & ResString("lg_phrase_registration_mail1") & " " & ResString("lg_domain") & ". " & ResString("lg_phrase_registration_mail2") & "<br>"
+        mailBody = mailBody & ResString("lg_phrase_registration_mail3") & "<br><br>"
+        mailBody = mailBody & "<a href=""http://" & ResString("lg_domain") & ResString("lg_loginPath") & ResString("lg_verify_page") & "?token=" & Token & "&p=" & Destination & """>" & ResString("lg_phrase_registration_mail4") & "</a><br><br>"
+        mailBody = mailBody & ResString("lg_phrase_registration_mail5")
+        mailBody = mailBody & ResString("lg_domain") & ResString("lg_loginPath") & ResString("lg_verify_page") & "</p>"
+        mailBody = mailBody & ResString("lg_phrase_registration_mail6") & ".<br><br>"
+        mailBody = mailBody & Token & "<br><br>"
+        mailBody = mailBody & ResString("lg_phrase_registration_mail7") & " "
+        mailBody = mailBody & ResString("lg_phrase_registration_mail8") & ResString("lg_domain") & ResString("lg_loginPath") & ResString("lg_register_delete_page") & "?email=" & Email & """>" & ResString("lg_term_remove_registration") & "</a>.<br><br>"
+        mailBody = mailBody & ResString("lg_phrase_registration_mail9") & ResString("lg_domain") & ResString("lg_contact_form") & """>" & ResString("lg_phrase_contact_webmaster") & "</a>.<br><br>"
+        mailBody = mailBody & ResString("lg_copyright") & "<br>"
+        mailBody = mailBody & "</FONT></DIV></BODY></HTML>"
+
+
+        SendMail(ConfigurationManager.AppSettings("lg_webmaster_email"), Email, "User Registration", mailBody)
+        SendMail(ConfigurationManager.AppSettings("lg_webmaster_email"), "rod@rodsdot.com", "ATTN:Webmaster User Registration", mailBody)
+
+    End Sub
+
+    Public Shared Sub SendMail(ByVal FromAddress As String, ByVal ToAddress As String, ByVal Subject As String, ByVal Body As String)
+        'Configuration in 
+        Dim Client As New SmtpClient
+        Client.Send(FromAddress, ToAddress, Subject, Body)
+    End Sub
+
+End Class
---
+++ VB.NET/Login-System/Login-System/Entropy.vb
@@ -0,0 +1,80 @@
+﻿
+
+Public Class Entropy
+    Public Shared Function getEntropy(ByVal pPass As String) As Double
+        Dim lowLetters, upLetters, symbols, digits As String
+        Dim totalChars, lowLettersChars, upLettersChars, symbolChars, digitChars, otherChars As Integer
+
+
+        lowLetters = "abcdefghijklmnopqrstuvwxyz"
+        upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        symbols = "~`!@#$%^&*()-_+="
+        digits = "1234567890"
+
+        totalChars = 95
+        lowLettersChars = Len(lowLetters)
+        upLettersChars = Len(upLetters)
+        symbolChars = Len(symbols)
+        digitChars = Len(digits)
+        otherChars = totalChars - (lowLettersChars + upLettersChars + symbolChars + digitChars)
+
+
+        Dim hasLower, hasUpper, hasSymbol, hasDigit As Boolean
+        Dim hasOther As Boolean, idx As Integer, c As Char, match As Boolean, domain As Integer
+
+        If (Len(pPass) < 0) Then
+            getEntropy = 0
+        Else
+            hasLower = False
+            hasUpper = False
+            hasSymbol = False
+            hasDigit = False
+            hasOther = False
+            domain = 0
+
+            For idx = 0 To pPass.Length - 1
+                c = pPass.Chars(idx)
+                match = False
+
+                If InStr(lowLetters, c) > 0 Then
+                    hasLower = True
+                    match = True
+                End If
+                If InStr(upLetters, c) > 0 Then
+                    hasUpper = True
+                    match = True
+                End If
+                If InStr(digits, c) > 0 Then
+                    hasDigit = True
+                    match = True
+                End If
+                If InStr(symbols, c) > 0 Then
+                    hasSymbol = True
+                    match = True
+                End If
+                If match = False Then
+                    hasOther = True
+                End If
+            Next
+
+            If (hasLower) Then
+                domain = domain + lowLettersChars
+            End If
+            If (hasUpper) Then
+                domain = domain + upLettersChars
+            End If
+            If (hasDigit) Then
+                domain = domain + digitChars
+            End If
+            If (hasSymbol) Then
+                domain = domain + symbolChars
+            End If
+            If (hasOther) Then
+                domain = domain + otherChars
+            End If
+
+            Return Int(Math.Log(domain) * (Len(pPass) / Math.Log(2)))
+
+        End If
+    End Function
+End Class
